d1282e954b72086a251791a750a740a3903a719e
Geoff Smith <smith260194@gmail.com>
2019-05-26 07:07:46 +0100

    Adding entry_station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,13 @@
 class Oystercard
   MAXIMUM_BALANCE = 90
-  OVER_MAX_BALANCE_ERROR = 'Top up not allowed. Max balance exceeded.'.freeze
-  attr_reader :balance
+  MINIMUM_FARE = 1
+  OVER_MAX_BALANCE_ERROR = 'Top up not allowed. Maximum balance exceeded.'
+  BAL_UNDER_MIN_FARE = 'Touch in now allowed. Minimum balance not met.'
+  attr_reader :balance, :entry_station
   def initialize(balance, in_journey = false)
     @balance = balance
     @in_journey = in_journey
+    @entry_station = nil
   end
 
   def top_up(amount)
@@ -12,23 +15,30 @@ class Oystercard
     @balance += amount
   end
 
-  def deduct(amount)
-    @balance -= amount
-  end
-
   def in_journey?
     @in_journey
   end
 
   def touch_in
+    check_card_has_minimum_fare
+
     @in_journey = true
   end
 
   def touch_out
     @in_journey = false
+    deduct(MINIMUM_FARE)
   end
 
   private
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def check_card_has_minimum_fare
+    raise BAL_UNDER_MIN_FARE if @balance < MINIMUM_FARE
+  end
 
   def check_new_balance(amount)
     raise OVER_MAX_BALANCE_ERROR if @balance + amount > MAXIMUM_BALANCE
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,6 +6,10 @@ describe Oystercard do
     it 'has a balance of 0 by default when initialized' do
       expect(subject.balance).to eq(0)
     end
+
+    it 'has an entry station instance variable that is nil' do
+      expect(subject.entry_station).to eq(nil)
+    end
   end
 
   describe '#top_up' do
@@ -16,14 +20,6 @@ describe Oystercard do
     it 'will raise an error if new balance will exceed the maximum' do
       expect { subject.top_up(Oystercard::MAXIMUM_BALANCE + 1) }
         .to raise_error(Oystercard::OVER_MAX_BALANCE_ERROR)
-    end
-  end
-
-  describe '#deduct' do
-    it 'can deduct a specified amount from the card' do
-      subject.top_up(50)
-
-      expect { subject.deduct(5) }.to change { subject.balance }.by(-5)
     end
   end
 
@@ -41,17 +37,25 @@ describe Oystercard do
 
   describe '#touch_in' do
     it 'can change the state of in_journey from false to true' do
+      subject = Oystercard.new(Oystercard::MINIMUM_FARE)
+
       subject.touch_in
 
       expect(subject.in_journey?).to eq(true)
     end
 
     it 'in_journey will remain true if touched in' do
-      subject = Oystercard.new(0, true)
+      subject = Oystercard.new(Oystercard::MINIMUM_FARE, true)
 
       subject.touch_in
 
       expect(subject.in_journey?).to eq(true)
+    end
+
+    it 'will raise an error if card is touched in without minimum fare' do
+      subject.top_up(Oystercard::MINIMUM_FARE - 1)
+
+      expect { subject.touch_in }.to raise_error(Oystercard::BAL_UNDER_MIN_FARE)
     end
   end
 
@@ -68,6 +72,10 @@ describe Oystercard do
       subject.touch_out
 
       expect(subject.in_journey?).to eq(false)
+    end
+
+    it 'will reduce the balance by the minimum fare on touch out' do
+      expect { subject.touch_out }.to change { subject.balance }.by(-Oystercard::MINIMUM_FARE)
     end
   end
 end
