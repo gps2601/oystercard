37883f42a685b73ddc0848f1e8ec3088154187fd
Geoff Smith <smith260194@gmail.com>
2019-05-26 05:49:36 +0100

    Rubocop config

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:my_oystercard) { Oystercard.new }
+  it 'has a balance of 0 by default when initialized' do
+    expect(my_oystercard.balance).to eq(0)
+  end
 end
