af396c149bf12b19260aafd04ceb92fca6c88478
Geoff Smith <smith260194@gmail.com>
2019-05-26 06:00:21 +0100

    Passing in balance as parameter to initialize

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,6 @@
 class Oystercard
   attr_reader :balance
-  def initialize
-    @balance = 0
+  def initialize(balance)
+    @balance = balance
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
-  let(:subject) { Oystercard.new }
+  let(:subject) { Oystercard.new(0) }
   describe '#initialize' do
     it 'has a balance of 0 by default when initialized' do
       expect(subject.balance).to eq(0)
